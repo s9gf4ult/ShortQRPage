d2bb8c9e0a414722e9cb7f6ad485ffeb33da5108
segfault <s9gf4ult@gmail.com>
2012-06-22 16:26:14 +0600

    modified:   ShortQRPage.php

--- ShortQRPage.php
+++ ShortQRPage.php
@@ -1,12 +1,4 @@
 <?php
-# Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
-if (!defined('MEDIAWIKI')) {
-        echo <<<EOT
-'To install my extension, put the following line in LocalSettings.php:
-require_once( "\$IP/extensions/ShortQRPage/ShortQRPage.php" );'
-          EOT;
-        exit( 1 );
-}
 
 $wgExtensionCredits['specialpage'][] = array('path' => __FILE__,
                                              'name' => 'ShortQRPage',
